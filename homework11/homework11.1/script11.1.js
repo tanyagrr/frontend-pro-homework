603e5fbc13ca5d188b8de125f1f80f7f8e68193b
root <tanyagera2003@gmail.com>
2025-10-12 13:27:58 +0300

    Add: hw 11.1-11.3

---
+++ homework11/homework11.1/script11.1.js
@@ -0,0 +1,79 @@
+const rowOne = document.querySelector(".row.one");
+for (let i = 1; i < 11; i++) {
+const rowOneResult = document.createElement("div");
+rowOneResult.classList.add("number-vert");
+rowOneResult.textContent = `${i}`;
+rowOne.appendChild(rowOneResult);
+};
+
+const rowTwo = document.querySelector(".row.two");
+for (let i = 1; i < 11; i++) {
+const rowTwoResult = document.createElement("div");
+rowTwoResult.classList.add("number-vert");
+rowTwoResult.textContent = `${i * 2}`;
+rowTwo.appendChild(rowTwoResult);
+};
+
+const rowThree = document.querySelector(".row.three");
+for (let i = 1; i < 11; i++) {
+const rowThreeResult = document.createElement("div");
+rowThreeResult.classList.add("number-vert");
+rowThreeResult.textContent = `${i * 3}`;
+rowThree.appendChild(rowThreeResult);
+};
+
+const rowFour = document.querySelector(".row.four");
+for (let i = 1; i < 11; i++) {
+const rowFourResult = document.createElement("div");
+rowFourResult.classList.add("number-vert");
+rowFourResult.textContent = `${i * 4}`;
+rowFour.appendChild(rowFourResult);
+};
+
+const rowFive = document.querySelector(".row.five");
+for (let i = 1; i < 11; i++) {
+const rowFiveResult = document.createElement("div");
+rowFiveResult.classList.add("number-vert");
+rowFiveResult.textContent = `${i * 5}`;
+rowFive.appendChild(rowFiveResult);
+};
+
+const rowSix = document.querySelector(".row.six");
+for (let i = 1; i < 11; i++) {
+const rowSixResult = document.createElement("div");
+rowSixResult.classList.add("number-vert");
+rowSixResult.textContent = `${i * 6}`;
+rowSix.appendChild(rowSixResult);
+};
+
+const rowSeven = document.querySelector(".row.seven");
+for (let i = 1; i < 11; i++) {
+const rowSevenResult = document.createElement("div");
+rowSevenResult.classList.add("number-vert");
+rowSevenResult.textContent = `${i * 7}`;
+rowSeven.appendChild(rowSevenResult);
+};
+
+const rowEight = document.querySelector(".row.eight");
+for (let i = 1; i < 11; i++) {
+const rowEightResult = document.createElement("div");
+rowEightResult.classList.add("number-vert");
+rowEightResult.textContent = `${i * 8}`;
+rowEight.appendChild(rowEightResult);
+};
+
+const rowNine = document.querySelector(".row.nine");
+for (let i = 1; i < 11; i++) {
+const rowNineResult = document.createElement("div");
+rowNineResult.classList.add("number-vert");
+rowNineResult.textContent = `${i * 9}`;
+rowNine.appendChild(rowNineResult);
+};
+
+const rowTen = document.querySelector(".row.ten");
+for (let i = 1; i < 11; i++) {
+const rowTenResult = document.createElement("div");
+rowTenResult.classList.add("number-vert");
+rowTenResult.textContent = `${i * 10}`;
+rowTen.appendChild(rowTenResult);
+};
---
+++ homework11/homework11.3/script11.3.js
@@ -0,0 +1,3 @@
+const randomId = "" + Math.floor((Math.random() * 9) + 1);
+const picToShow = document.getElementById(randomId);;
+picToShow.classList.add("show-pic");
