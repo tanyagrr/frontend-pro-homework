13e19f1bce075cc0fd222f5c7b1c3125460fa48b
root <tanyagera2003@gmail.com>
2025-10-13 00:25:01 +0300

    Fix: hw 11.1, 11.3

--- homework11/homework11.3/script11.3.js
+++ homework11/homework11.3/script11.3.js
@@ -1,3 +1,8 @@
-const randomId = "" + Math.floor((Math.random() * 9) + 1);
-const picToShow = document.getElementById(randomId);;
-picToShow.classList.add("show-pic");
+const imageElement = document.querySelector(".pic");
+const randomId = `./assets/puppy${Math.floor((Math.random() * 9) + 1)}.jpg`;
+
+imageElement.setAttribute("src", `${randomId}`);
+
+
+// const picToShow = document.getElementById(randomId);;
+// picToShow.classList.add("show-pic");
